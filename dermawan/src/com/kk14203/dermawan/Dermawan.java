852806863c5f31ef9fef944c4808c0900d6e5051
Azrin Aziz <azrinaziz@gmail.com>
2016-03-13 21:00:51 +0800

    Update copyright

--- dermawan/src/com/kk14203/dermawan/Dermawan.java
+++ dermawan/src/com/kk14203/dermawan/Dermawan.java
@@ -1,3 +1,12 @@
+/*
+ * Name:        Dermawan.java
+ * Created:     13-3-2016
+ * Copyright:   (c) 2016 Azrin Aziz <azrinaziz@gmail.com>
+ * Limitations: For educational purposes only. Any unauthorised copying of any
+ *              part of this source code without the authors' explicit
+ *              permission is strictly prohibited.
+ */
+
 package com.kk14203.dermawan;
 
 public class Dermawan {
