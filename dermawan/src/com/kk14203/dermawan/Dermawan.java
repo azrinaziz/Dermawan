eef4a85ba1071744a6feb2a3d93bfef69c1031d2
Ez Simon Takom <ezra5071@gmail.com>
2016-03-16 12:18:02 +0800

    Add main menu

--- dermawan/src/com/kk14203/dermawan/Dermawan.java
+++ dermawan/src/com/kk14203/dermawan/Dermawan.java
@@ -8,16 +8,44 @@
 
 package com.kk14203.dermawan;
 
+import java.util.Scanner;
+
 final public class Dermawan {
 
-    private void execute() {
-        System.out.println("Dermawan");
-        System.out.println("KK14203");
-        System.out.println("Development branch test");
-    }
+    private void execute()//used to output details about the program
+        {
+            System.out.println("Dermawan");
+            System.out.println("KK14203");
+            System.out.println("This program stores list of donors and also enable user to view, delete and edit donor details");
+        }
+
+
+    private void menu()//provides menu for users to choose from
+        {
+            System.out.println("(1) - Add donor");
+            System.out.println("(2) - Delete donor");
+            System.out.println("(3) - Edit donor");
+            System.out.println("(4) - View Donor");
+            System.out.println("Please enter between 1 - 4. Enter 0 to exit");
+        }
+
+
+
+
 
     public static void main(String[] args) {
+
         Dermawan dermawan = new Dermawan();
         dermawan.execute();
+        int input;
+        Scanner reader = new Scanner(System.in); //used to get input from user
+
+
+        do {
+            dermawan.menu();
+            input = reader.nextInt();//reads the input as an int and put it into input variable
+        }while(input!=0);
     }
+
+
 }
