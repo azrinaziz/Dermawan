d840683c836932cd2df8952039504829e130b354
Azrin Aziz <azrinaziz@gmail.com>
2016-03-18 19:16:57 +0800

    Code change

--- dermawan/src/com/kk14203/dermawan/Utility.java
+++ dermawan/src/com/kk14203/dermawan/Utility.java
@@ -8,5 +8,41 @@
 
 package com.kk14203.dermawan;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 final class Utility {
+
+    static List<Object> loadFile(File file, Object object, boolean createNewFile) throws IOException {
+        /*
+        List<Object> donatorsList = new ArrayList<>();
+
+        if (file.exists()) {
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            if (file.length() > 0) {
+                try (ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+                    donatorsList = (List<Object>) objectInputStream.readObject();
+                }
+                catch (ClassNotFoundException e) {
+                    return null;
+                }
+            }
+
+            fileInputStream.close();
+            return donatorsList;
+
+        } else {
+            if (createNewFile) {
+                if (file.createNewFile())
+                    return donatorsList;
+                else
+                    return null;
+            } else
+                return null;
+        }
+        */
+        return null;
+    }
 }
