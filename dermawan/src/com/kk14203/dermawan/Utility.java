e9db11a1c3210cc31e64f6f5f3e66ea7f1f4eb8e
Azrin Aziz <azrinaziz@gmail.com>
2016-03-18 12:04:32 +0800

    Add Utility class

---
+++ dermawan/src/com/kk14203/dermawan/Utility.java
@@ -0,0 +1,12 @@
+/*
+ * Name:        Utility.java
+ * Copyright:   (c) 2016 Azrin Aziz <azrinaziz@gmail.com>
+ * Limitations: For educational purposes only. Any unauthorised copying of any
+ *              part of this source code without the authors' explicit
+ *              permission is strictly prohibited.
+ */
+
+package com.kk14203.dermawan;
+
+final class Utility {
+}
